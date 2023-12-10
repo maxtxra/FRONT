ebe8ff3a69c3b27d631d23e5fdf33634d9a4fc5f
Max <39965015+maxtxra@users.noreply.github.com>
2023-12-10 13:25:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-background'), // ID do contêiner
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://lottie.host/51625968-6ddf-47a8-bc7a-9285284daba6/rCvJREIo1l.json' // Caminho do arquivo Lottie
+  });
+  
